e6b7527053ef7803f19ba75dd0b01e215cf821eb
ssondh <blue951208@naver.com>
2025-09-21 22:21:22 +0900

    react-router-dom, css 추가

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ function App() {
     <>
       <div className="App">
         <h1>4년차 개발자 손동현입니다.</h1>
+        <hr className="intro-line1"/>
         {/*<header className="App-header">*/}
         {/*  <img src={logo} className="App-logo" alt="logo" />*/}
         {/*  <p>*/}
@@ -28,22 +29,41 @@ function App() {
           <h3>Email. blue951208@naver.com</h3>
         </div>
         <div className="short-intro">
-          <h2>끝까지 책임지는 4년 차 개발자 손동현입니다.</h2>
-          <p>
-            처음 접하는 기술 스택도 주저하지 않고
-            직접 학습하며 프로젝트에 적용해왔습니다.
-            익숙하지 않은 환경에서도 책임감을 바탕으로 끝
-            까지 완수해온 경험이 제 강점입니다.
-          </p>
+            <div>
+                <h2>끝까지 책임지는 4년 차 개발자<br/>손동현입니다.</h2>
+                <p>
+                    처음 접하는 기술 스택도 주저하지 않고
+                    직접 학습하며 프로젝트에 적용해왔습니다.
+                    익숙하지 않은 환경에서도 책임감을 바탕으로 끝
+                    까지 완수해온 경험이 제 강점입니다.
+                </p>
+            </div>
+            <div className="skills">
+                <h2>스킬</h2>
+                <ul>
+                    <li>Frontend
+                        <ul>
+                            <li>jsp</li>
+                            <li>jquery</li>
+                            <li>Javascript</li>
+                        </ul>
+                    </li>
+                    <li>Backend
+                        <ul>
+                            <li>Java</li>
+                            <li>Spring Framework</li>
+                        </ul>
+                    </li>
+                    <li>DB
+                        <ul>
+                            <li>PostgreSQL</li>
+                            <li>Oracle</li>
+                            <li>MariaDB</li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
         </div>
-      </div>
-      <div className="skills">
-        <h2>스킬</h2>
-        <ul>
-          <li>Frontend : jsp, jquery, Javascript</li>
-          <li>Backend : Java, Spring Framework</li>
-          <li>DB : PostgreSQL, Oracle, MariaDB</li>
-        </ul>
       </div>
     </>
   );
