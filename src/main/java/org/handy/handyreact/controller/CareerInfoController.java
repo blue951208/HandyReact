a69d17292a47778f070ecaaeed06abd268310c5e
ssondh <blue951208@naver.com>
2026-03-10 18:50:04 +0900

    Mybatis 설정 추가

---
+++ src/main/java/org/handy/handyreact/controller/CareerInfoController.java
@@ -0,0 +1,10 @@
+package org.handy.handyreact.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/career-info")
+public class CareerInfoController {
+
+}
---
+++ src/main/java/org/handy/handyreact/mapper/CareerInfoMapper.java
@@ -0,0 +1,8 @@
+package org.handy.handyreact.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface CareerInfoMapper {
+
+}
